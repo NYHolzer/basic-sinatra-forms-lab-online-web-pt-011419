ced619d69d92bb99924b6acd8152539c8c27031c
Nissan Holzer <nissan.holzer@gmail.com>
2019-05-10 00:28:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
   
   post '/team' do
+      @name = params["name"]
+      @coach = params["coach"]
+      @pg = params["pg"]
+      @sg = params["sg"]></p>
+      <p> Small Forward: <input type="text" name="sf"></p>
+      <p> Power Forward: <input type="text" name="pf"></p>
+      <p> Center: <input type="text" name="c"></p>
     erb :team
   end 
 end
